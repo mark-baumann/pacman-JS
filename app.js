c016cbf54fb5660420d55519cd0a7be7820554e1
Mark Baumann <60445235+mark-baumann@users.noreply.github.com>
2021-05-31 12:41:43 +0200

    eat dots

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()  => {
 const grid = document.querySelector('.grid')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 const width = 28
 
 //0 = pacman
@@ -43,12 +43,15 @@ const layout = [
   const squares = []
 
 function createBoard() {
+  //gehe über jedes Element des Layouts
   for (let i = 0; i < layout.length; i++) {
+
     const square = document.createElement('div')
+
     grid.appendChild(square)
     squares.push(square)
 
-    //add layout to the board
+    //weise das Oben stehende Array den CSS Propertys auf dem Grid zu
     if(layout[i] === 0) {
       squares[i].classList.add('pac-dot')
     } else if (layout[i] === 1) {
@@ -61,6 +64,81 @@ function createBoard() {
   }
 }
 createBoard()
+
+
+//wo Pac Man gerade ist und füge Ihn in Sqaures ein
+let pacmanCurrentIndex = 490
+squares[pacmanCurrentIndex].classList.add('pac-man')
+
+function movePacman(e) {
+//  entferne als erstes Pacman
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+    switch(e.keyCode) {
+      //Pfeiltaste links
+      case 37:
+      //wenn die aktuelle Position von Pacman nicht einer Wand oder dem Ghost Lair entspricht, dann -1
+        if(
+        pacmanCurrentIndex % width !== 0 &&
+          !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')
+          )
+        pacmanCurrentIndex -= 1
+        break
+      //Pfeiltaste Oben
+      case 38:
+      //
+        if(
+          pacmanCurrentIndex - width >= 0 &&
+          !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')
+          )
+        //dann subtrahiere etwas von der Höhe
+        pacmanCurrentIndex -= width
+        break
+      //Pfeiltaste Rechts
+      case 39:
+        if(
+          pacmanCurrentIndex % width < width - 1 &&
+          !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')
+        )
+        pacmanCurrentIndex += 1
+        break
+        //Pfeiltaste unten
+      case 40:
+        if (
+          pacmanCurrentIndex + width < width * width &&
+          !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')
+        )
+        //addiere etwas zur Höhe
+        pacmanCurrentIndex += width
+        break
+    }
+    //füge Pacman dann wieder hinzu, jetzt aber mit dem neuen Algorithmus
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+    pacDotEaten()
+  }
+  //event Listener für die Tastensteuerung
+  document.addEventListener('keyup', movePacman)
+
+
+  function pacDotEaten() {
+      if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+        score++
+        scoreDisplay.innerHTML = score
+        squares[pacmanCurrentIndex].classList.remove('pac-dot')
+      }
+    }
+
+  class Ghost {
+    constructor(className, startIndex, speed) {
+
+    }
+  }
+
+
+
 
 
 
